c4b6b5445ea3511877f8de838f8d87da7df47c3a
David <david.rigaux@eisti.fr>
2017-10-27 11:15:59 +0200

    Initial commit

---
+++ confirm.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+# Import modules for CGI handling
+import cgi, cgitb
+
+# Create instance of FieldStorage
+form = cgi.FieldStorage()
+
+# Get data from fields
+nom = form.getvalue('nom')
+prenom  = form.getvalue('prenom')
+age = form.getvalue('age')
+pseudo = form.getvalue('pseudo')
+
+def verifUnique(nom,prenom,age,pseudo):
+    f = open('db','r')
+    lineAjoute = nom+";"+prenom+";"+pseudo+";"+age
+    for line in f:
+        if (line == lineAjoute):
+            return (False)
+
+
+print ("""Content-type:text/html\r\n\r\n
+<html>
+    <head>
+        <title>Confirmation</title>
+        <meta http-equiv='content-type' content='text/html;charset=UTF-8'>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+    </head>
+    <body>
+    
+    <div class="row">
+        <div class="container-fluid col-10">
+        <h1>Bienvenu !</h1>
+            <h2>Bonjour %s %s, A.K.A %s</h2>
+            <p>Vous avez %s ans</p>
+               <div class="row">
+                   <form action="users.py">
+                        <div class="form-group col-6">
+                            <button type="submit" value="Jetter un coup d'oeil aux utilisateurs" class="btn btn-primary">Jetter un oeil aux utilisateurs</button>
+                        </div>
+                    </form>
+                </div>
+                
+            </form>
+
+        </div>
+        </div>
+    </body>
+</html>"""% (prenom, nom,pseudo,age))
---
+++ index.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+# coding=utf-8
+print ("""Content-type:text/html\r\n\r\n
+<html>
+    <head>
+        <title>Welcome</title>
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    </head>
+    <body>
+        <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="./index.py">POC</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="./index.py">Accueil<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./users.py">Utilisateurs</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<div class="container-fluid">
+<h1>Bienvenu dans notre POC</h1>
+    <div class="row">
+        <div class="container-fluid col-6">
+            <form action="confirm.py" method="get">
+                <div class="row">
+                    <div class="form-group col-6">
+                        <label for="prenom">Prenom :</label>
+                        <input type="text" class="form-control" id="prenom" name="prenom" placeholder="Prenom">
+                    </div>
+    
+                    <div class="form-group col-6">
+                        <label for="nom">Nom :</label>
+                        <input type="text" class="form-control" id="nom" name="nom" placeholder="Nom">
+                    </div>
+               
+                </div>
+                 <div class="row">
+                 <div class="form-group col-6">
+                        <label for="pseudo">Pseudo :</label>
+                        <input type="text" class="form-control" id="pseudo" name="pseudo" placeholder="Pseudo">
+                    </div>
+                 
+                <div class="form-group col-4">
+                    <label for="age">Age :</label>
+                    <input type="text" class="form-control" id="age" name="age" placeholder="Age">
+                </div>
+                </div>
+               <div class="row">
+                <div class="form-group col-6">
+                    <button type="submit" value="submit" class="btn btn-primary">Submit</button>
+                </div>
+                </div>
+                
+            </form>
+
+        </div>
+        </div>
+        </div>
+    </body>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+</html>""")
