2f41f761dbf2fefd9b4302043094cb30b669872c
David RIGAUX <davidrigaux88@gmail.com>
2017-10-27 22:19:37 +0200

    Version Rendue

--- users.py
+++ users.py
@@ -14,34 +14,33 @@ print ("""Content-type:text/html\r\n\r\n
         <link rel="shortcut icon" href="img/logo.png">
     </head>
     <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="./index.py">
-    <img src="img/logo.png" width="30" height="30" alt="">
-  </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="./index.py">Accueil</a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="./users.py">Utilisateurs<span class="sr-only">(current)</span></a>
-      </li>
-    </ul>
-  </div>
-</nav>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="./index.py">
+                <img src="img/logo.png" width="30" height="30" alt="">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="./index.py">Accueil</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="./users.py">Utilisateurs<span class="sr-only">(current)</span></a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
         <div class="container-fluid col-6">
-        <h1>Utilisateurs</h1>
-        <table class="table table-striped">
-            <tr>
-                <th>Nom</th>
-                <th>Prenom</th>
-                <th>Pseudonyme</th>
-                <th>Age</th>
-            </tr>
-       """)
+            <h1>Utilisateurs</h1>
+                <table class="table table-striped">
+                    <tr>
+                        <th>Nom</th>
+                        <th>Prenom</th>
+                        <th>Pseudonyme</th>
+                        <th>Age</th>
+                    </tr>""")
 
 for line in f:
     print("<tr>")
@@ -50,10 +49,10 @@ for line in f:
     print("</tr>")
 
 print("""
-    </table>
-    </div>
+                </table>
+        </div>
     </body>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 </html>""")
